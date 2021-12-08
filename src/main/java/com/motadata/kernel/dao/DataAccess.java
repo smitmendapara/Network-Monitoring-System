8be58f53a22ed341f875a39b2c1c4ba6e3bf176a
Smit Mendapara <69449110+smitmendapara@users.noreply.github.com>
2021-12-08 19:09:43 +0530

    Add files via upload

---
+++ src/main/java/com/motadata/kernel/dao/DataAccess.java
@@ -0,0 +1,11 @@
+package com.motadata.kernel.dao;
+
+import org.eclipse.jetty.server.ConnectionFactory;
+
+/**
+ * Created by smit on 8/12/21.
+ */
+public class DataAccess
+{
+    ConnectionFactory connectionFactory 
+}
---
+++ src/main/java/com/motadata/kernel/util/CommonConstant.java
@@ -0,0 +1,13 @@
+package com.motadata.kernel.util;
+
+import java.io.File;
+
+/**
+ * Created by smit on 7/12/21.
+ */
+public class CommonConstant
+{
+    public static final String CURRENT_DIR = System.getProperty("user.dir");
+
+    public static final String PATH_SEPARATOR = File.separator;
+}
