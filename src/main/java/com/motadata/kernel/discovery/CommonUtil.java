5a5c6642eb0064f86b33926e2b4e8173ac1ba28e
Smit Mendapara <69449110+smitmendapara@users.noreply.github.com>
2022-01-04 20:38:55 +0530

    Add files via upload

---
+++ src/main/java/com/motadata/kernel/discovery/CommonUtil.java
@@ -0,0 +1,34 @@
+package com.motadata.kernel.discovery;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class CommonUtil
+{
+    private final static LinkedBlockingQueue<String> pollingQueue = new LinkedBlockingQueue<>();
+
+    public static String takePollingIp()
+    {
+        try
+        {
+            return pollingQueue.take();
+        }
+        catch (Exception exception)
+        {
+            exception.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void putPollingIp(String ipName)
+    {
+        try
+        {
+            pollingQueue.add(ipName);
+        }
+        catch (Exception exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+}
